799606e0c300cf2220fd90526d6942075fb3faf1
christianmonson <christianrmonson@gmail.com>
2015-01-27 20:51:25 -0700

    completes project

--- ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
+++ ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
@@ -18,6 +18,25 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
     // Insert code here...
+    int Karl = 10;
+    int Leonard = 7;
+    int karlPlace = Karl > 7 ? 1 : 2;
+    int leonardPlace = Leonard > 7 ? 1 : 2;
+    
+
+    if (karlPlace == 1 && leonardPlace == 1) {
+        NSLog(@"Both teachers are placed 1");
+    }
+    else if (karlPlace == 1 && leonardPlace == 2){
+        NSLog(@"Karl is placed 1 and Leonard is placed 2");
+    }
+    else if (karlPlace == 2 && leonardPlace == 1){
+        NSLog(@"Leonard is placed 1 and Karl is placed 2");
+    }
+    else if (karlPlace == 2 && leonardPlace == 2){
+        NSLog(@"Karl and Leonard are placed 2");
+    }
+    
     
     
     return YES;
